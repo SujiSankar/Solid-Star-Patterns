45938ad6e565719abc273a4c4fb65a09dde3fafe
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:29:50 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String args[]) {
+      int i,j,n=6;
+      for(i=1;i<=n;i++)
+      {
+          for(j=i;j<=n;j++)
+          {
+              System.out.print(" ");
+          }
+          for(j=0;j<=i;j++)
+          {
+              System.out.print("* ");
+              
+          }
+          System.out.println();
+      }
+    }
+}
+Output:
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
