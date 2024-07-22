cd5d7958a175accab36553f6d81ae5abc909ae57
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:22:37 +0530

    Create Right increasing triangle.java

---
+++ Right increasing triangle.java
@@ -0,0 +1,22 @@
+public class Main   
+{   
+ public static void main(String args[]) 
+int i, j, row=4;    
+for(i=1; i<=row; i++)   
+{   
+for(j=1; j<=i; j++)   
+{   
+ 
+System.out.print("* ");   
+}   
+System.out.println();   
+}   
+}   
+}  
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
