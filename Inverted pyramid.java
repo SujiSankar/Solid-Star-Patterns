c2718b9b836bb66e6406304ad8087c7cfe725873
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:30:44 +0530

    Create Inverted pyramid.java

---
+++ Inverted pyramid.java
@@ -0,0 +1,24 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+               System.out.print(" "); 
+            }
+            for(j=i;j<=n;j++)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+       
+    }
+}
+OUTPUT:
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
