8dfa224b0a5e9a8de937793266284133881483b0
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:27:28 +0530

    Create Right Decreasing triangle.java

---
+++ Right Decreasing triangle.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String args[]) {
+      int i,j,n=6;
+      for(i=1;i<=n;i++)
+      {
+          for(j=i;j<=n;j++)
+          {
+              System.out.print("*");
+          }
+          System.out.println();
+      }
+    }
+}
+
+Output:
+
+******
+*****
+****
+***
+**
+*
